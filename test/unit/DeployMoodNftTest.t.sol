b5af5ae635164dcb197703c94bc0af3dd759429f
cqlyj <luoyingjie0721@gmail.com>
2024-06-07 00:50:22 +0800

    chore: Refactor test file structure and add new folders

--- test/unit/DeployMoodNftTest.t.sol
+++ test/unit/DeployMoodNftTest.t.sol
@@ -18,9 +18,6 @@ contract DeployMoodNftTest is Test {
         string
             memory svg = '<svg viewBox="0 0 200 200" width="400"  height="400" xmlns="http://www.w3.org/2000/svg"><circle cx="100" cy="100" fill="yellow" r="78" stroke="black" stroke-width="3"/><g class="eyes"><circle cx="63" cy="82" r="12"/><circle cx="130" cy="82" r="12"/></g><path d="m136.81 116.53c.69 26.17-64.11 42-81.52-.73" style="fill:none; stroke: black; stroke-width: 3;"/></svg>';
         string memory actualUri = deployer.svgToImageURI(svg);
-        console.log(actualUri);
-        console.log("-----------------------");
-        console.log(expectedUri);
         assert(
             keccak256(abi.encodePacked(expectedUri)) ==
                 keccak256(abi.encodePacked(actualUri))
